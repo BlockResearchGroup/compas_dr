ea70e56de6b0886edce808fbf80a6a8edb253388
tomvanmele <tom.v.mele@gmail.com>
2024-02-29 22:53:28 +0100

    not all constraints included at this point

--- src/compas_dr/constraints/__init__.py
+++ src/compas_dr/constraints/__init__.py
@@ -5,8 +5,6 @@ from __future__ import print_function
 from __future__ import absolute_import
 from __future__ import division
 
-from compas.geometry import Vector
-from compas.geometry import Frame
 from compas.geometry import Line
 from compas.geometry import Plane
 from compas.geometry import Circle
@@ -15,20 +13,14 @@ from compas.geometry import NurbsSurface
 
 from .constraint import Constraint
 
-from .vectorconstraint import VectorConstraint
-from .frameconstraint import FrameConstraint
 from .lineconstraint import LineConstraint
 from .planeconstraint import PlaneConstraint
 from .circleconstraint import CircleConstraint
-
 from .curveconstraint import CurveConstraint
 from .surfaceconstraint import SurfaceConstraint
 
-Constraint.register(Vector, VectorConstraint)
-Constraint.register(Frame, FrameConstraint)
 Constraint.register(Line, LineConstraint)
 Constraint.register(Plane, PlaneConstraint)
 Constraint.register(Circle, CircleConstraint)
-
 Constraint.register(NurbsCurve, CurveConstraint)
 Constraint.register(NurbsSurface, SurfaceConstraint)
